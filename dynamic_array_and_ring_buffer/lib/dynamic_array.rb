96ce800bb5263368dae74467d2da5027a9d5b40b
Mike Salisbury <michael.salisbury87@gmail.com>
2018-02-21 22:10:57 -0800

    start dynamic_array

--- dynamic_array_and_ring_buffer/lib/dynamic_array.rb
+++ dynamic_array_and_ring_buffer/lib/dynamic_array.rb
@@ -4,37 +4,52 @@ class DynamicArray
   attr_reader :length
 
   def initialize
-
+    @store = []
+    @length = 0
+    @capacity = 8
   end
 
   # O(1)
   def [](index)
-
+    if @store[index]
+      @store[index]
+    else
+      raise "index out of bounds"
+    end
   end
 
   # O(1)
   def []=(index, value)
-
+    @store[index] = value
   end
 
   # O(1)
   def pop
-
+    popped = @store[-1]
+    @store = @store[0...-1]
+    @length -= 1
+    popped
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
-
+    @length += 1
+    @store[@length + 1] = val
   end
 
   # O(n): has to shift over all the elements.
   def shift
-
+    shifted = @store[0]
+    @store = @store[1..-1]
+    @length -= 1
+    shifted
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    @length += 1
+    @store = [val] + @store
 
   end
 
@@ -47,6 +62,6 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
-
+    
   end
 end
--- dynamic_array_and_ring_buffer/lib/static_array.rb
+++ dynamic_array_and_ring_buffer/lib/static_array.rb
@@ -1,5 +1,6 @@
 # This class just dumbs down a regular Array to be statically sized.
 class StaticArray
+
   def initialize(length)
     @store = Array.new(length)
   end
