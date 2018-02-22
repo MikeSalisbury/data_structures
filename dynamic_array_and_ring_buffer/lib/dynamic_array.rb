bd0c9f7105c4ebc45d0a5d6f20f8bb4e921206e9
Mike Salisbury <michael.salisbury87@gmail.com>
2018-02-21 22:34:18 -0800

    complete dynamic_array

--- dynamic_array_and_ring_buffer/lib/dynamic_array.rb
+++ dynamic_array_and_ring_buffer/lib/dynamic_array.rb
@@ -11,35 +11,41 @@ class DynamicArray
 
   # O(1)
   def [](index)
-    if @store[index]
-      @store[index]
-    else
-      raise "index out of bounds"
-    end
+    raise "index out of bounds" if index >= @length
+    return @store[index]
   end
 
   # O(1)
   def []=(index, value)
-    @store[index] = value
+      raise "index out of bounds" if index > @capacity
+      @store[index] = value
   end
 
   # O(1)
   def pop
-    popped = @store[-1]
-    @store = @store[0...-1]
-    @length -= 1
-    popped
+    if @length == 0
+      raise "index out of bounds"
+    else
+      popped = @store[-1]
+      @store = @store[0...-1]
+      @length -= 1
+      popped
+    end
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    if @capacity - @length == 0
+      resize!
+    end
+    @store[@length] = val
     @length += 1
-    @store[@length + 1] = val
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" if @length == 0
     shifted = @store[0]
     @store = @store[1..-1]
     @length -= 1
@@ -50,7 +56,6 @@ class DynamicArray
   def unshift(val)
     @length += 1
     @store = [val] + @store
-
   end
 
   protected
@@ -62,6 +67,11 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
-    
+    temp_arr = []
+    @store.each do |el|
+      temp_arr.push(el)
+    end
+    @capacity *= 2
+    @store = temp_arr
   end
 end
